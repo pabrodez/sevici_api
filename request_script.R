277f9feeadf959c948758a73459cf7ce4cdc0d48
pabrodez <pabrodrod1@alum.us.es>
2018-05-16 22:10:57 +0200

    Update request_script.R

--- request_script.R
+++ request_script.R
@@ -1,9 +1,6 @@
 # libraries
-library(jsonlite)
 library(tidyverse)
 library(httr)
-
-setwd("/home/aliens/Dropbox/R projects/sevici")
 
 if (!dir.exists("./decaux")) {
   dir.create("./decaux")
@@ -27,6 +24,6 @@ for (i in seq_along(5*2)) { # every 1/2 hour for 5 hours
 
   write(req_content, file = paste0("./decaux/", day_hour, ".json")) # Save request content
   
-  Sys.sleep(1800)
+  Sys.sleep(1800)  # put to sleep for 30 minutes between each iteration
 }
 
