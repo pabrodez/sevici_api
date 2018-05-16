d44570f4fca7d0eeedfe6e0ae2643f8b9e6851cc
pabrodez <pabrodrod1@alum.us.es>
2018-05-16 22:11:32 +0200

    Update request_script.R

--- request_script.R
+++ request_script.R
@@ -15,7 +15,7 @@ for (i in seq_along(5*2)) { # every 1/2 hour for 5 hours
       "https://api.jcdecaux.com/vls/v1/stations?contract=Seville&apiKey=90f096efd2e83e1711874c7a60324e41361b964b"
     )
 
-    Sys.sleep(300)
+    Sys.sleep(300)  # if request fails repeat every 5 minutes
   }
 
   req_content <- httr::content(request, as = "text", encoding = "UTF-8") # Get the content as text. keep json structure
